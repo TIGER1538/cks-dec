ec6b6ea2259c5af17396e4eba257a378bd1ef3b4
System Administrator <tiger1538@icloud.com>
2022-09-09 20:38:23 +0900

    improve efficiency

--- src/sample/info.rs
+++ src/sample/info.rs
@@ -21,17 +21,22 @@ pub struct SampleInfo {
 
 impl SampleInfo {
     pub fn new<R: Read + Seek>(mut reader: R) -> Self {
-        let format = read_to_u8(&mut reader);
-        let channels = read_to_u8(&mut reader);
-        let sample_rate = read_to_u16(&mut reader);
-        let blocks = read_to_i32(&mut reader);
-        let block_bytes = read_to_u16(&mut reader);
-        let block_frames = read_to_u16(&mut reader);
-        let volume = read_to_u16(&mut reader);
-        let pan = read_to_i16(&mut reader);
-        let loop_start = read_to_u32(&mut reader);
-        let loop_end = read_to_u32(&mut reader);
-        let loop_count = read_to_i16(&mut reader);
+        let mut buf = ReaderBuf {
+            buf_4bytes: [0, 0, 0, 0],
+            buf_2bytes: [0, 0],
+            buf_1byte: [0],
+        };
+        let format = read_to_u8(&mut reader, &mut buf);
+        let channels = read_to_u8(&mut reader, &mut buf);
+        let sample_rate = read_to_u16(&mut reader, &mut buf);
+        let blocks = read_to_i32(&mut reader, &mut buf);
+        let block_bytes = read_to_u16(&mut reader, &mut buf);
+        let block_frames = read_to_u16(&mut reader, &mut buf);
+        let volume = read_to_u16(&mut reader, &mut buf);
+        let pan = read_to_i16(&mut reader, &mut buf);
+        let loop_start = read_to_u32(&mut reader, &mut buf);
+        let loop_end = read_to_u32(&mut reader, &mut buf);
+        let loop_count = read_to_i16(&mut reader, &mut buf);
         let _ = reader.seek(SeekFrom::Current(2));
 
         let format = match format {
@@ -74,52 +79,58 @@ impl SampleInfo {
     }
 }
 
+struct ReaderBuf {
+    buf_4bytes: [u8; 4],
+    buf_2bytes: [u8; 2],
+    buf_1byte: [u8; 1],
+}
+
 #[inline]
-fn read_to_u8<R>(reader: &mut R) -> u8
+fn read_to_u8<R>(reader: &mut R, buf: &mut ReaderBuf) -> u8
 where
     R: Read + Seek,
 {
-    let mut buf = [0u8; 1];
-    let _ = (*reader).read_exact(&mut buf);
+    let mut buf = buf.buf_1byte;
+    let _ = reader.read_exact(&mut buf);
     buf[0]
 }
 
 #[inline]
-fn read_to_u16<R>(reader: &mut R) -> u16
+fn read_to_u16<R>(reader: &mut R, buf: &mut ReaderBuf) -> u16
 where
     R: Read + Seek,
 {
-    let mut buf = [0u8; 2];
+    let mut buf = buf.buf_2bytes;
     let _ = reader.read_exact(&mut buf);
     u16::from_le_bytes(buf)
 }
 
 #[inline]
-fn read_to_u32<R>(reader: &mut R) -> u32
+fn read_to_u32<R>(reader: &mut R, buf: &mut ReaderBuf) -> u32
 where
     R: Read + Seek,
 {
-    let mut buf = [0u8; 4];
+    let mut buf = buf.buf_4bytes;
     let _ = reader.read_exact(&mut buf);
     u32::from_le_bytes(buf)
 }
 
 #[inline]
-fn read_to_i16<R>(reader: &mut R) -> i16
+fn read_to_i16<R>(reader: &mut R, buf: &ReaderBuf) -> i16
 where
     R: Read + Seek,
 {
-    let mut buf = [0u8; 2];
+    let mut buf = buf.buf_2bytes;
     let _ = reader.read_exact(&mut buf);
     i16::from_le_bytes(buf)
 }
 
 #[inline]
-fn read_to_i32<R>(reader: &mut R) -> i32
+fn read_to_i32<R>(reader: &mut R, buf: &mut ReaderBuf) -> i32
 where
     R: Read + Seek,
 {
-    let mut buf = [0u8; 4];
+    let mut buf = buf.buf_4bytes;
     let _ = reader.read_exact(&mut buf);
     i32::from_le_bytes(buf)
 }
